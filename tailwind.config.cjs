602f30ed9ac5c0a84da161862c647c93db384a44
bobaekang <bkang.dev@gmail.com>
2022-04-12 18:25:42 -0500

    Customize tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,34 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      // custom colors
+      primary: '#42B7C1',
+      'primary-light': '#E1FFFF',
+      'primary-dark': '#008993',
+      secondary: '#312B71',
+      'secondary-light': '#11468F',
+      background: '#0F0D22',
+
+      // tailwind colors
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+    },
+    extend: {
+      backgroundImage: {
+        splash:
+          "linear-gradient(0deg, rgba(49 43 113 / 54%), rgba(0 137 147 / 69%)), url('/splash.jpg')",
+      },
+      fontFamily: {
+        sans: ['Gothic A1', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 }
